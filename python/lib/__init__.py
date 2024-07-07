77e72ee46f51d668b5c0351de563399fa6a6830f
Mimoja <git@mimoja.de>
2024-07-07 19:40:58 +0200

    pypi: use post-release versioning schemme instead of guess-next
    Signed-off-by: Mimoja <git@mimoja.de>

--- python/lib/__init__.py
+++ python/lib/__init__.py
@@ -3,14 +3,20 @@ from importlib.resources import files, as_file
 
 try:
     from setuptools_scm import get_version
+    import pathlib
 
-    real_version = get_version(root="..")
-    pretend_version = get_version(root="..", local_scheme="no-local-version")
+    root = pathlib.Path(__file__).parent.parent.parent.resolve()
+    real_version = get_version(root=root, version_scheme="post-release")
+    pretend_version = get_version(
+        root=root, version_scheme="post-release", local_scheme="no-local-version"
+    )
     if pretend_version != real_version:
-        __version__ = pretend_version
+        __version__ = pretend_version.replace("post", "")
     else:
         __version__ = real_version
-except Exception:
+    print(f"sqlite-zstd version: {__version__}")
+except Exception as e:
+    print(f"Failed to get sqlite-zstd version: {e}")
     __version__ = "0.0.0"
 
 
@@ -18,3 +24,6 @@ def load(conn: sqlite3.Connection) -> None:
     lib = next(x for x in files(__name__).iterdir() if x.name.startswith("lib"))
     with as_file(lib) as ext:
         conn.load_extension(str(ext))
+
+
+print(__version__)
